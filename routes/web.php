145ba574cbce88354db69617e217c87df894d2fd
umaedi <umaedi.kh.99@gmail.com>
2025-09-24 15:51:50 +0700

    fix: comment out unused route and navbar links for future reference

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Web\TentangController;
-use App\Http\Controllers\Web\LhkpnController;
-use App\Http\Controllers\Web\DokumenController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\OTPController;
+namespace App\Http\Controllers;
 
-Route::view('/', 'web.home.index');
+use Illuminate\Support\Facades\Route;
+
+Route::view('/','web.home.index');
 
 Route::prefix('p')->group(function() {
-    Route::get('/tugas-fungsi', [TentangController::class, 'tugasFungsi'])->name('web.tugas-fungsi');
-    Route::get('/profil-perusahaan', [TentangController::class, 'profil'])->name('web.profil');
-    Route::get('/biodata-komisaris', [TentangController::class, 'biodataKomisaris'])->name('web.biodata-komisaris');
-    Route::get('/biodata-direktur', [TentangController::class, 'biodataDirektur'])->name('web.biodata-direktur');
+    // Route::get('/tugas-fungsi', [Web\TentangController::class, 'tugasFungsi'])->name('web.tugas-fungsi');
+    Route::get('/profil-perusahaan', [Web\TentangController::class, 'profil'])->name('web.profil');
+    Route::get('/biodata-komisaris', [Web\TentangController::class, 'biodataKomisaris'])->name('web.biodata-komisaris');
+    Route::get('/biodata-direktur', [Web\TentangController::class, 'biodataDirektur'])->name('web.biodata-direktur');
 
-    Route::get('/lhkpn', [LhkpnController::class, 'index'])->name('web.lhkpn');
+    Route::get('/lhkpn', [Web\LhkpnController::class, 'index'])->name('web.lhkpn');
 });
 
-Route::get('/dokumen', [DokumenController::class, 'index'])->name('web.dokumen');
+// Route::get('/dokumen', [Web\DokumenController::class, 'index'])->name('web.dokumen');
 
-// route auth
-Route::post('/user/login', LoginController::class)->middleware('guest')->name('login');
+//route auth
+Route::post('/user/login', Auth\LoginController::class)->middleware('guest')->name('login');
 
 Route::middleware('auth')->group(function () {
-    Route::controller(OTPController::class)->group(function () {
+    Route::controller(Auth\OTPController::class)->group(function () {
         Route::get('/otp', 'otp');
         Route::post('/verify-otp', 'verifyOtp');
     });
