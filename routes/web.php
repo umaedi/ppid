032352f70491919f4d4492eac3df4e97ef6a1ba5
umaedi <umaedi.kh.99@gmail.com>
2025-09-24 15:52:46 +0700

    fix: uncomment routes for 'tugas-fungsi' and 'dokumen' for proper functionality

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/','web.home.index');
 
 Route::prefix('p')->group(function() {
-    // Route::get('/tugas-fungsi', [Web\TentangController::class, 'tugasFungsi'])->name('web.tugas-fungsi');
+    Route::get('/tugas-fungsi', [Web\TentangController::class, 'tugasFungsi'])->name('web.tugas-fungsi');
     Route::get('/profil-perusahaan', [Web\TentangController::class, 'profil'])->name('web.profil');
     Route::get('/biodata-komisaris', [Web\TentangController::class, 'biodataKomisaris'])->name('web.biodata-komisaris');
     Route::get('/biodata-direktur', [Web\TentangController::class, 'biodataDirektur'])->name('web.biodata-direktur');
@@ -15,7 +15,7 @@ Route::prefix('p')->group(function() {
     Route::get('/lhkpn', [Web\LhkpnController::class, 'index'])->name('web.lhkpn');
 });
 
-// Route::get('/dokumen', [Web\DokumenController::class, 'index'])->name('web.dokumen');
+Route::get('/dokumen', [Web\DokumenController::class, 'index'])->name('web.dokumen');
 
 //route auth
 Route::post('/user/login', Auth\LoginController::class)->middleware('guest')->name('login');
